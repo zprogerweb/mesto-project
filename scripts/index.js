dad6c9480198d202045eb70922d95368e89e99f7
zproger <zprogerweb@yandex.ru>
2022-08-02 12:49:17 +0300

    popupImage

--- scripts/index.js
+++ scripts/index.js
@@ -83,37 +83,49 @@ function createCard(cardName, cardLink) {
   
   cardElement.querySelector('.element__title').textContent = cardName;
   cardElement.querySelector('.element__image').src = cardLink;
+
   cardElement.querySelector('.element__heart').addEventListener('click', function (evt) {
     evt.target.classList.toggle('element__heart_active');
   });
-
+  // Добавляем кнопку удаления карточки
   const deleteButton = cardElement.querySelector('.element__basket');
   deleteButton.addEventListener('click', function () {
   const elementItem = deleteButton.closest('.element');
   elementItem.remove();
   });
+  const elementImage = cardElement.querySelector('.element__image');
+  const imageCardPopup = document.querySelector('.image-card');
+
+  // Передаем значения карточки в попап просмотра картинки
+  elementImage.addEventListener ('click', function(evt) {
+    evt.preventDefault();
+    const popupImage = imageCardItem.querySelector('.image-card__image');
+    const popupCaption = imageCardItem.querySelector('.image-card__caption');
+    popupCaption.textContent = cardName;
+    popupImage.src = cardLink;
+    imageCardPopup.classList.add('image-card_opened');
+    })
+    
   return cardElement;
 } 
 
-
-
+// Вставляем карточку вначало списка
 function renderCard(card) {
   elements.prepend(card);
 }
 
-
-
+// Открытие попапа для добавления новой карточки
 function addClickButton() {
   popupElement.classList.add('popup_opened');
 }
 addButton.addEventListener('click', addClickButton);
 
-
+// Закрытие попапа добавление карточки на крестик
 closeIconElement.addEventListener('click', function() {
   popupElement.classList.remove('popup_opened');
 });
 
-
+// Закрытие попапа добавления карточки на кнопку "Сохранить" 
   function handleAddCardClick(evt) {
     evt.preventDefault();
     
@@ -123,7 +135,6 @@ closeIconElement.addEventListener('click', function() {
     let newcard = createCard(photoTitleInput, photoLinkInput);
     renderCard(newcard);
     evt.target.reset ();
-
   }
   formElement.addEventListener('submit', handleAddCardClick);
   
@@ -133,15 +144,18 @@ closeIconElement.addEventListener('click', function() {
   }
   submitElement.addEventListener('click', submitFormElement);
 
+  // Попап картинки
+  const imageCardPopup = document.querySelector('.image-card');
+  const imageCardItem = imageCardPopup.querySelector('.image-card__item');
+  const popupImage = imageCardItem.querySelector('.image-card__image');
+  const popupCaption = imageCardItem.querySelector('.image-card__caption');
+  const closeIconImage = imageCardItem.querySelector('.popup__close-icone_image');
+  const elementImage = elements.querySelector('.element__image');
+  
 
-  
-  
-    
-  
-  // Удаление карточки
+
+  closeIconImage.addEventListener('click', function() {
+    imageCardPopup.classList.remove('image-card_opened');
+});
  
-
-
-  cardElement.querySelector('.element__heart').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('element__heart_active');
-  });
+ 
